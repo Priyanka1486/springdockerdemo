8d9fee02675dbfb406e6af4108fd567d6f9a58af
Priyanka1486 <mishrapriyanka14@gmail.com>
2022-05-30 01:31:09 +0530

    change commit

--- src/main/java/com/controller/SpringDockerDemoApplication.java
+++ src/main/java/com/controller/SpringDockerDemoApplication.java
@@ -2,12 +2,14 @@ package com.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringDockerDemoApplication {
 
+	@GetMapping("/")
 	public String home() {
 		return "Welcome to Spring Demo";
 	}
